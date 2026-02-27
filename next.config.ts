f2f1ed197aa2ce76ae1379e7a0b0c84d7f9f7a74
danad1821 <danadabdoub@gmail.com>
2026-02-27 10:34:29 +0200

    adjusted config

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // serverExternalPackages: ['mongoose'],
-  // output: 'standalone',
   experimental: {
-    // 2. serverActions is now stable in Next 14+, 
+    // 2. serverActions is now stable in Next 14+,
     // but bodySizeLimit still lives here if you need to increase it
     serverActions: {
-      bodySizeLimit: '50mb',
-      allowedOrigins: ['altamaritime.com', 'www.altamaritime.com', 'springgreen-hedgehog-502712.hostingersite.com', 'www.springgreen-hedgehog-502712.hostingersite.com']
+      bodySizeLimit: "50mb",
+      allowedOrigins: [
+        "altamaritime.com",
+        "www.altamaritime.com",
+        "https://altamaritime.com",
+        "https://www.altamaritime.com",
+      ],
     },
   },
-  
-
 };
 
 export default nextConfig;
