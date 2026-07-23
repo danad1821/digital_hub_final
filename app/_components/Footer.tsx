4a83dae11f0df31ee47888403068e10e3d004479
danad1821 <danadabdoub@gmail.com>
2026-07-23 11:56:41 +0300

    white

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -73,7 +73,7 @@ export default function Footer() {
             {/* Logo Block */}
             <div className="flex items-center gap-2">
               <Image
-                src="/images/ultamaritime.svg"
+                src="/ultamaritimeWhite.svg"
                 alt="alta maritime logo"
                 width={200}
                 height={100}
